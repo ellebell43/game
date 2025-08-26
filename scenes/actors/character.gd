a3fd96b6ef1b1193cf0dcc429556749857c0d806
Elle Brooks <ellebell43@proton.me>
2025-08-26 14:48:33 -0700

    jumping and falling animations

--- scenes/actors/character.gd
+++ scenes/actors/character.gd
@@ -29,7 +29,7 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		jumpPlayer.play()
-		
+		sprite.play("jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -45,9 +45,13 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
-	if velocity.x != 0:
+	if velocity.x != 0 and velocity.y == 0:
 		sprite.play("run")
-	else:
+	elif velocity.x == 0 and velocity.y == 0:
 		sprite.play("idle")
+	elif velocity.y < 0:
+		pass
+	elif velocity.y > 250:
+		sprite.play("fall")
 
 	move_and_slide()
