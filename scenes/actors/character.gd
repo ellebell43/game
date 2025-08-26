9e0471ff4b7f5fc8c06a6b5f517f8dc540186fe3
Elle Brooks <ellebell43@proton.me>
2025-08-26 13:52:18 -0700

    camera, sky, and jumping
    - Player now has a camera that follows it and bounds can be set vie the Inspector
    - Jumping plays a sound
    - Sky background image
    - Modified the tilemap layer to have collisions and removed all the manually set collision shapes

--- scenes/actors/character.gd
+++ scenes/actors/character.gd
@@ -1,10 +1,23 @@
 extends CharacterBody2D
 
 @onready var sprite = $AnimatedSprite2D
+@onready var jumpPlayer = $JumpAudioPLayer
+@onready var camera = $Camera2D
+
+@export var cameraLimitTop = 500
+@export var cameraLimitBottom = 500
+@export var cameraLimitLeft = 500
+@export var cameraLimitRight = 500
 
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -250.0
+
+func _ready() -> void:
+	camera.limit_bottom = cameraLimitBottom
+	camera.limit_top = cameraLimitTop
+	camera.limit_right = cameraLimitRight
+	camera.limit_left = cameraLimitLeft
 
 
 func _physics_process(delta: float) -> void:
@@ -15,6 +28,8 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jumpPlayer.play()
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
