a5f505358faaebea5183129764c1f19d4b907fb1
Elle Brooks <ellebell43@proton.me>
2025-08-26 15:57:39 -0700

    better idle animation and crouching
    - Animation on jump, fall, and crouch
    - If the character sits for 3 seconds, they play an idle animation.
    - If crouched, they drop to the ground and cannot move

--- scenes/actors/character.gd
+++ scenes/actors/character.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @onready var sprite = $AnimatedSprite2D
 @onready var jumpPlayer = $JumpAudioPLayer
 @onready var camera = $Camera2D
+@onready var idleTimer = $IdleTimer
 
 @export var cameraLimitTop = 500
 @export var cameraLimitBottom = 500
@@ -26,31 +27,42 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor() and !Input.is_action_pressed("crouch"):
+		idleTimer.start()
 		velocity.y = JUMP_VELOCITY
 		jumpPlayer.play()
 		sprite.play("jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	
 	if direction > 0:
 		sprite.flip_h = false
 	elif direction < 0:
 		sprite.flip_h = true
-		
-	if direction:
-		velocity.x = direction * SPEED
+	
+	if direction and not Input.is_action_pressed("crouch"):
+		idleTimer.start()
+		if Input.is_action_pressed("sprint"):
+			velocity.x = direction * SPEED * 1.5
+		else:
+			velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	if velocity.x != 0 and velocity.y == 0:
-		sprite.play("run")
-	elif velocity.x == 0 and velocity.y == 0:
+		velocity.x = move_toward(velocity.x, 0, 10)
+	
+	#ANIMATION TREE
+	if Input.is_action_pressed("crouch"):
+		sprite.play("crouch")
+	elif velocity.x != 0 and velocity.y == 0:
+		if Input.is_action_pressed("sprint"):
+			sprite.play("walk")
+		else:
+			sprite.play("run")
+	elif velocity.x == 0 and velocity.y == 0 and idleTimer.is_stopped():
 		sprite.play("idle")
-	elif velocity.y < 0:
-		pass
+	elif velocity.x == 0 and velocity.y == 0:
+		sprite.play("stand")
 	elif velocity.y > 250:
 		sprite.play("fall")
 
