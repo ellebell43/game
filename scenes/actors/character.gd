2c4fd0775b5056de297d6499cdb98a4ed440b550
Elle Brooks <ellebell43@proton.me>
2025-08-26 21:52:17 -0700

    spikes and death
    - new spike tiles that exist on collision layer 3 (named hazards)
    - game over sound file
    - Working to make the player jump up then fall out of frame when dead

--- scenes/actors/character.gd
+++ scenes/actors/character.gd
@@ -1,19 +1,26 @@
 extends CharacterBody2D
 
+# node references
 @onready var sprite = $AnimatedSprite2D
 @onready var jumpPlayer = $JumpAudioPLayer
 @onready var camera = $Camera2D
 @onready var idleTimer = $IdleTimer
+@onready var gameOverPlayer = $GameOverPlayer
+@onready var worldCollision = $WorldCollision
 
+# exported variables
 @export var cameraLimitTop = 500
 @export var cameraLimitBottom = 500
 @export var cameraLimitLeft = 500
 @export var cameraLimitRight = 500
 
-
+# constants
 const SPEED = 100.0
 const JUMP_VELOCITY = -250.0
 
+var dead = false
+
+# on ready, set camera limits
 func _ready() -> void:
 	camera.limit_bottom = cameraLimitBottom
 	camera.limit_top = cameraLimitTop
@@ -28,31 +35,36 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor() and !Input.is_action_pressed("crouch"):
+		#reset idle timer, move up, play a sound, and play an animation
 		idleTimer.start()
 		velocity.y = JUMP_VELOCITY
 		jumpPlayer.play()
 		sprite.play("jump")
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
 	
+	# flip sprite depending on movement direction
 	if direction > 0:
 		sprite.flip_h = false
 	elif direction < 0:
 		sprite.flip_h = true
 	
+	# While a direction is held, move unless crouched
 	if direction and not Input.is_action_pressed("crouch"):
 		idleTimer.start()
 		if Input.is_action_pressed("sprint"):
 			velocity.x = direction * SPEED * 1.5
 		else:
 			velocity.x = direction * SPEED
+	# If no direction is held, slow down and stop
 	else:
 		velocity.x = move_toward(velocity.x, 0, 10)
 	
-	#ANIMATION TREE
-	if Input.is_action_pressed("crouch"):
+	# Animation tree
+	if dead:
+		pass
+	elif Input.is_action_pressed("crouch"):
 		sprite.play("crouch")
 	elif velocity.x != 0 and velocity.y == 0:
 		if Input.is_action_pressed("sprint"):
@@ -67,3 +79,14 @@ func _physics_process(delta: float) -> void:
 		sprite.play("fall")
 
 	move_and_slide()
+
+
+func _on_hit_box_body_entered(body: Node2D) -> void:
+	var dead = true
+	if dead :
+		gameOverPlayer.play()
+		sprite.play("dead")
+		velocity.y = (JUMP_VELOCITY / 2)
+		sprite.modulate = Color(1, 0.5, 0.5)
+		set_process_input(false)
+		worldCollision.disabled = true
